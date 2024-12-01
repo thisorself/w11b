e8030c67b48e77542a8e67e08bc3ffc2354b5748
thisorself <reservposhtaclassroom@gmail.com>
2024-12-01 04:21:03 +0300

    task20

--- index.php
+++ index.php
@@ -9,25 +9,49 @@
     <?php
     include 'config.php';
 
-    $estate_id = 5;
-    $estate_address = '"' . $mysqli->real_escape_string("Україна, Вінниця, площа Костянтина Моглика 24, кв. 75") . '"';
-    $estate_property_type = $mysqli->real_escape_string("Квартира");
-    $estate_deal_type = $mysqli->real_escape_string("Продаж");
-    $estate_area = 45;
-    $estate_description = '"' . $mysqli->real_escape_string("Продається квартира. Середня кухня, маленька зала, окремо туалет та ванна кімната, є кондиціонер і бойлер. Одне одномісне ліжко, дві шафи.") . '"';
-    $estate_owner_fullname = '"' . $mysqli->real_escape_string("Мрійко Марина Ігорівна") . '"';
-    $estate_owner_telephone = '"' . $mysqli->real_escape_string("+380358746582") . '"';
-    $estate_owner_email = null;
-    $estate_price = 5000000;
+    $estate_id1 = 6;
+    $estate_address1 = '"' . $mysqli->real_escape_string("123") . '"';
+    $estate_property_type1 = $mysqli->real_escape_string("Будинок");
+    $estate_deal_type1 = $mysqli->real_escape_string("Продаж");
+    $estate_area1 = 1;
+    $estate_description1 = '"' . $mysqli->real_escape_string("456") . '"';
+    $estate_owner_fullname1 = '"' . $mysqli->real_escape_string("789") . '"';
+    $estate_owner_telephone1 = $mysqli->real_escape_string("001");
+    $estate_owner_email1 = "exampl1";
+    $estate_price1 = 125;
 
-    $query = "INSERT INTO real_estates (id, address, property_type, deal_type, area, description, owner_fullname, owner_telephone, owner_email, price) 
-                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    $statement = $mysqli->prepare($query);
-    $statement->bind_param('isssissssi', $estate_id, $estate_address, $estate_property_type, $estate_deal_type, $estate_area, $estate_description,
-                                         $estate_owner_fullname, $estate_owner_telephone, $estate_owner_email, $estate_price);
+    $estate_id2 = 7;
+    $estate_address2 = '"' . $mysqli->real_escape_string("456") . '"';
+    $estate_property_type2 = $mysqli->real_escape_string("Квартира");
+    $estate_deal_type2 = $mysqli->real_escape_string("Оренда");
+    $estate_area2 = 2;
+    $estate_description2 = '"' . $mysqli->real_escape_string("789") . '"';
+    $estate_owner_fullname2 = '"' . $mysqli->real_escape_string("002") . '"';
+    $estate_owner_telephone2 = $mysqli->real_escape_string("125");
+    $estate_owner_email2 = "exampl2";
+    $estate_price2 = 625;
 
-    if ($statement->execute()) {
-        print 'Запит успішно виконаний! ID останнього вставленого запису: ' . $statement->insert_id . '<br />';
+    $estate_id3 = 8;
+    $estate_address3 = '"' . $mysqli->real_escape_string("789") . '"';
+    $estate_property_type3 = $mysqli->real_escape_string("Будинок");
+    $estate_deal_type3 = $mysqli->real_escape_string("Оренда");
+    $estate_area3 = 1;
+    $estate_description3 = '"' . $mysqli->real_escape_string("003") . '"';
+    $estate_owner_fullname3 = '"' . $mysqli->real_escape_string("625") . '"';
+    $estate_owner_telephone3 = $mysqli->real_escape_string("551");
+    $estate_owner_email3 = "exampl3";
+    $estate_price3 = 3125;
+
+    $insert = $mysqli->query("INSERT INTO real_estates (id, address, property_type, deal_type, area, description, owner_fullname, owner_telephone, owner_email, price) 
+                              VALUES ($estate_id1, '$estate_address1', '$estate_property_type1', '$estate_deal_type1', $estate_area1, '$estate_description1',
+                                      '$estate_owner_fullname1', '$estate_owner_telephone1', '$estate_owner_email1', $estate_price1),
+                                      ($estate_id2, '$estate_address2', '$estate_property_type2', '$estate_deal_type2', $estate_area2, '$estate_description2',
+                                         '$estate_owner_fullname2', '$estate_owner_telephone2', '$estate_owner_email2', $estate_price2),
+                                         ($estate_id3, '$estate_address3', '$estate_property_type3', '$estate_deal_type3', $estate_area3, '$estate_description3',
+                                         '$estate_owner_fullname3', '$estate_owner_telephone3', '$estate_owner_email3', $estate_price3)");
+
+    if ($insert) {
+        print 'Запит успішно виконаний! ID останнього вставленого запису: ' . $mysqli->affected_rows . '<br />';
     } else {
         die('Помилка: (' . $mysqli->errno . ') ' . $mysqli->error);
     }
