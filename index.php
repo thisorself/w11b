6b52bcd69aee78a0320e64c46bb5b6caad86f40f
thisorself <reservposhtaclassroom@gmail.com>
2024-12-01 03:40:11 +0300

    task16

--- index.php
+++ index.php
@@ -9,12 +9,36 @@
     <?php
     include 'config.php';
 
-    $results = $mysqli->query("SELECT COUNT(*) FROM real_estates");
-    $rows_count = $results->fetch_row();
+    $estate_deal_type = 'Продаж';
 
-    print $rows_count[0];
+    $query = "SELECT id, address, property_type, deal_type, area, description, 
+                     owner_fullname, owner_telephone, owner_email, price 
+              FROM real_estates WHERE deal_type=?";
+    $statement = $mysqli->prepare($query);
 
-    $mysqli->close();
+    $statement->bind_param('s', $estate_deal_type);
+    $statement->execute();
+    $statement->bind_result($id, $address, $property_type, $deal_type, $area, $description,
+                            $owner_fullname, $owner_telephone, $owner_email, $price);
+
+    print '<table border="1">';
+    while ($statement->fetch()) {
+        print '<tr>';
+        print '<td>' . $id . '</td>';
+        print '<td>' . $address . '</td>';
+        print '<td>' . $property_type . '</td>';
+        print '<td>' . $deal_type . '</td>';
+        print '<td>' . $area . '</td>';
+        print '<td>' . $description . '</td>';
+        print '<td>' . $owner_fullname . '</td>';
+        print '<td>' . $owner_telephone . '</td>';
+        print '<td>' . $owner_email . '</td>';
+        print '<td>' . $price . '</td>';
+        print '</tr>';
+    }
+    print '</table>';
+
+    $statement->close();
     ?>
 </body>
 
