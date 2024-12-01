1f2adcfee53a6f7b5ed8b50ec6f6dd434ae4b9f1
thisorself <reservposhtaclassroom@gmail.com>
2024-12-01 03:17:34 +0300

    task14

--- index.php
+++ index.php
@@ -9,28 +9,10 @@
     <?php
     include 'config.php';
 
-    $results = $mysqli->query("SELECT id, address, property_type, deal_type, area, description, 
-                                             owner_fullname, owner_telephone, owner_email, price 
-                                      FROM real_estates");
+    $description = $mysqli->query("SELECT description FROM real_estates WHERE id = 2")->fetch_object()->description;
 
-    print '<table border="1">';
-    while ($row = $results->fetch_object()) {
-        print '<tr>';
-        print '<td>' . $row->id . '</td>';
-        print '<td>' . $row->address . '</td>';
-        print '<td>' . $row->property_type . '</td>';
-        print '<td>' . $row->deal_type . '</td>';
-        print '<td>' . $row->area . '</td>';
-        print '<td>' . $row->description . '</td>';
-        print '<td>' . $row->owner_fullname . '</td>';
-        print '<td>' . $row->owner_telephone . '</td>';
-        print '<td>' . $row->owner_email . '</td>';
-        print '<td>' . $row->price . '</td>';
-        print '</tr>';
-    }
-    print '</table';
+    print $description;
 
-    $results->free();
     $mysqli->close();
     ?>
 </body>
